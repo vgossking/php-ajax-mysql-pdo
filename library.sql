2dac1711340c90aa717f81909cd2c44c10b3c7b1
Vu Vuong <vu.vuong@niteco.se>
2017-02-09 17:54:03 +0700

    update sql file

--- library.sql
+++ library.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
+-- version 4.5.2
+-- http://www.phpmyadmin.net
 --
--- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th2 06, 2017 lúc 05:23 SA
--- Phiên bản máy phục vụ: 5.7.14
--- Phiên bản PHP: 5.6.25
+-- Host: 127.0.0.1
+-- Generation Time: Feb 09, 2017 at 10:53 AM
+-- Server version: 5.7.9
+-- PHP Version: 5.6.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,88 +17,91 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `library`
+-- Database: `library`
 --
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `books`
+-- Table structure for table `books`
 --
 
-CREATE TABLE `books` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `books`;
+CREATE TABLE IF NOT EXISTS `books` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `author` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `publisher` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `quantity` int(11) NOT NULL,
-  `categoryId` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `categoryId` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Đang đổ dữ liệu cho bảng `books`
+-- Dumping data for table `books`
 --
 
 INSERT INTO `books` (`id`, `title`, `author`, `publisher`, `quantity`, `categoryId`) VALUES
-(2, 'Than dieu dai hiep', 'Kim Dung', 'Kim Dong', 12, 1),
-(3, 'Thien Long Bat Bo', 'Kim Dung', 'Van hoc', 12, 1),
-(4, 'co gai van chuong', 'sdfs', 'Thai Ha', 2, 1),
+(27, 'King of war', 'kua of gink', 'sdfsd', 3, 3),
+(3, 'Thien Long Bat Bo', 'Kim Dung', 'Van hoc', 104, 1),
+(4, 'co gai van chuong', 'Mizuki', 'Thai Ha', 3, 1),
 (5, 'Kimi no nawa', 'Matoko Shinkai', 'IPM', 4, 4),
-(8, 'Duc dan don', '1232', '2ed1dr', 2, 4),
-(12, 'Hiep Khach Zang Ho', 'Kim Dung', 'Kim Dong', 20, 1),
-(13, 'Konichiwa', 'Kim Dung', 'Kim Dong', 20, 2);
+(14, 'Sherlock Holmes', 'Conan Doyle', 'Van Hoc', 4, 2),
+(19, 'rá»«ng na uy', 'sdfsd', 'sdfs', 23, 4),
+(13, 'Konichiwa', 'Kim Dung', 'Kim Dong', 20, 2),
+(15, 'Anh hung xa dieu', 'Kim dung', 'VH', 4, 1),
+(16, 'Clean Code', 'Uncle Bob', 'vh cntt', 4, 5),
+(17, 'Clean Coder', 'Uncle Bob', 'vh cntt', 5, 5),
+(18, 'Khu vuong ngon tinh', 'Makoto Shinkai', 'IPM', 2, 4),
+(21, 'Ä‘áº¯c nhÃ¢n tÃ¢m', '12', '213', 2, 5),
+(26, 'ahihi', 'hihia', 'hiahi', 23, 5),
+(24, 'ronaldo', 'ronaldo', 'ronaldo', 3, 1);
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `categories`
+-- Table structure for table `categories`
 --
 
-CREATE TABLE `categories` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Đang đổ dữ liệu cho bảng `categories`
+-- Dumping data for table `categories`
 --
 
 INSERT INTO `categories` (`id`, `name`) VALUES
 (1, 'Kiem hiep'),
 (2, 'Trinh tham'),
 (3, 'Ngon tinh'),
-(4, 'Tieu thuyet');
+(4, 'Tieu thuyet'),
+(5, 'CNTT');
+
+-- --------------------------------------------------------
 
 --
--- Chỉ mục cho các bảng đã đổ
+-- Table structure for table `users`
 --
 
---
--- Chỉ mục cho bảng `books`
---
-ALTER TABLE `books`
-  ADD PRIMARY KEY (`id`);
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Chỉ mục cho bảng `categories`
---
-ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT cho các bảng đã đổ
+-- Dumping data for table `users`
 --
 
---
--- AUTO_INCREMENT cho bảng `books`
---
-ALTER TABLE `books`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
---
--- AUTO_INCREMENT cho bảng `categories`
---
-ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3');
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
